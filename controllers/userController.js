4dc23625db0825f3a89c0ce397f5d59d346cf6d4
Flunder620 <jguzman12310@yahoo.com>
2023-11-29 23:43:47 -0600

    created get and create routes

--- controllers/userController.js
+++ controllers/userController.js
@@ -1,5 +1,36 @@
 const { User, Thought } = require ('../models');
 
 module.exports = {
-    
+    // Getting all users
+    async getUsers (req,res) {
+        try {
+            const users = await User.find();
+            res.json(users)
+        } catch (err) {
+            res.status(500).json(err);
+        }
+    },
+    // Getting single user
+    async getSingleUser(req,res) {
+        try {
+            const user = await User.findOne({ _id: req.params.userid})
+            .select('-__v');
+
+            if (!user) {
+                return res.status(404).json({ message:'No user found with that id'})
+            }
+            res.json(user)
+        } catch (err){
+            res.status(500).json(err)
+        }
+    },
+    // Creating a user
+    async createUser(req, res) {
+        try{
+            const user = await User.create(req.body);
+            res.json(user);
+        } catch (err){
+            res.statis(500).json(err);
+        }
+    }
 }
